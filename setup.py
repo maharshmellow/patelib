f3f659bc90c409c4cea7861a9b4031449929da4e
Maharsh Patel <maharshmellow@users.noreply.github.com>
2016-04-19 21:46:42 -0600

    added sorting package

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(name='patelib',
       author='Maharsh Patel',
       author_email='maharsh@live.ca',
       license='MIT',
-      packages=['patelib', 'patelib.number'],
+      packages=['patelib', 'patelib.number', 'patelib.sorting'],
       zip_safe=False)
