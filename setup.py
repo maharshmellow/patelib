29b62285aed24c2ce0daaa6f11c9a1fe110a5643
maharshmellow <maharsh@live.ca>
2016-05-07 19:19:17 -0600

    changed version number

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='patelib',
-      version='0.4.2',
+      version='0.4.3',
       description='Collection of Random Useful Stuff',
       url='http://github.com/maharshmellow/patelib',
       author='Maharsh Patel',
       author_email='patel@maharsh.net',
       license='MIT',
-      packages=['patelib', 'patelib.number', 'patelib.sorting', 'patelib.adt', 'patelib.file', 'patelib.searching', 'patelib.strings', 'patelib.images'],
+      packages=['patelib', 'patelib.number', 'patelib.sorting', 'patelib.adt', 'patelib.file', 'patelib.searching', 'patelib.strings', 'patelib.images', 'patelib.others'],
       zip_safe=False)
