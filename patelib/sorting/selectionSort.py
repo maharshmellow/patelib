881459c85172a8521c6289b5b15ed5f929d6e852
maharshmellow <maharsh@live.ca>
2016-05-08 15:30:53 -0600

    added selection sort complexities

--- patelib/sorting/selectionSort.py
+++ patelib/sorting/selectionSort.py
@@ -1,4 +1,12 @@
 def selectionSort(data):
+    """
+    Time Complexity
+        Best: O(n^2)
+        Average: O(n^2)
+        Worst: O(n^2)
+    Space Complexity
+        Worst: O(1)
+    """
     for index in range(len(data)):
         smallIndex = index
         # finding smallest
