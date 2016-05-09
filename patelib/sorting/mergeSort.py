a799444d09d2cee3e4addd134b0fe302b71f5d07
maharshmellow <maharsh@live.ca>
2016-05-09 13:11:58 -0600

    added merge sort

---
+++ patelib/sorting/mergeSort.py
@@ -0,0 +1,36 @@
+def merge(left, right):
+    result = []
+    i,j = 0,0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i+=1
+            
+        else:
+            result.append(right[j])
+            j+=1
+
+    # adding the remaining values from either list to the result
+    result += left[i:]
+    result += right[j:]
+    return result
+
+def mergeSort(data):
+    """
+    Time Complexity
+        Best: nlog(n)
+        Average: nlog(n)
+        Worst: nlog(n)
+    Space Complexity
+        Worst: O(n)
+    """
+    if len(data) <= 1:
+        return data
+    
+    middle = len(data)//2
+
+    left = mergeSort(data[:middle])
+    right = mergeSort(data[middle:])
+
+    return merge(left, right)
+print(mergeSort([10, 1, 29, 3, 59, 1, 12, 0, 12, 2, 3]))
