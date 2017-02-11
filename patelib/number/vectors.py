771921aa2d035d7fdd06048a43b8195b931ee869
Muchen He <mansurhe@outlook.com>
2017-02-11 01:15:47 -0800

    updated constructor and added unit vector function

--- patelib/number/vectors.py
+++ patelib/number/vectors.py
@@ -1,17 +1,22 @@
 import math
 
 class Vector2D:
-    def __init__(self):
-        self.__x=0
-        self.__y=0
+    def __init__(self, x, y):
+        self.__x=x
+        self.__y=y
+
     def x(self):
         return self.__x
+
     def y(self):
         return self.__y
+
     def mag(self):
         return math.sqrt(self.__x**2+self.__y**2)
+
     def msgSq(self):
         return self.__x**2+self.__y**2
+
     def add(self, v):
         if isinstance(v, Vector2D):
             self.__x+=v.x()
@@ -19,22 +24,29 @@ class Vector2D:
         else:
             self.__x+=v
             self.__y+=v
+
     def sub(self, v):
         self.add(-v)
+
     def mul(self, v):
         if isinstance(v, Vector2D):
             print("patelib>number>vectors>Vector2D>mul(): Scalar only; use .dot()")
         else:
             self.__x*=v;
             self.__y*=v;
+
     def div(self, v):
         if isinstance(v, Vector2D):
             print("patelib>number>vectors>Vector2D>div(): Scalar only")
         else:
             self.mul(-v)
+
     def dot(self, v):
         if not isinstance(v, Vector2D):
             print("patelib>number>vectors>Vector2D>dot(): Must be between two 2D vectors")
         else:
             return self.__x*v.x()+self.__y*v.y()
-    def 
+
+    def unit(self):
+        m=mag()
+        return Vector2D(self.__x/m, self.__y/m)
