1c78e3152328b7e71fa6ca8e5e6a579f16c3cca0
Muchen He <mansurhe@outlook.com>
2017-02-11 01:02:12 -0800

    added componenent and mag. getters

--- patelib/number/vectors.py
+++ patelib/number/vectors.py
@@ -4,4 +4,14 @@ class vector2D:
     def __init__(self):
         self.__x = 0
         self.__y = 0
-    
+    def x(self):
+        return self.__x
+    def y(self):
+        return self.__y
+    def mag(self):
+        return math.sqrt(self.__x**2+self.__y**2)
+    def msgSq(self):
+        return self.__x**2+self.__y**2
+    def add(self, v):
+        self.__x += v.x()
+        self.__y += v.y()
